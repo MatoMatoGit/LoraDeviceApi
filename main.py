06e0233491b609170998f91ee03e6e5956dd7c66
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2020-08-19 02:11:36 +0200

    * main: Call Report.SetOutputChannels properly.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import render_template
 import connexion
-import report
+import Report
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./config')
@@ -20,5 +20,6 @@ def home():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    SetOutputChannels({'data': ['uplink.data'], 'meta': ['uplink.meta']}
+    Report.SetOutputChannels({'data': ['uplink.data'], 'meta': ['uplink.meta'],
+        'url': ['downlink.url']})
     app.run(host='0.0.0.0', port=5000, debug=True)
